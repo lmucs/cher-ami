0dbb893432c30864f90e015cfc11e98012808f81
whusted <whusted@lion.lmu.edu>
2014-09-09 18:00:07 -0700

    First iteration of CherAmi with successful HTTP request logged to console

---
+++ ios/CherAmi/CherAmi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CherAmi
+//
+//  Created by Willy Husted on 9/9/14.
+//  Copyright (c) 2014 401. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ios/CherAmi/CherAmi/testHTTPViewController.h
@@ -0,0 +1,13 @@
+//
+//  testHTTPViewController.h
+//  CherAmi
+//
+//  Created by Willy Husted on 9/9/14.
+//  Copyright (c) 2014 401. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testHTTPViewController : UIViewController
+
+@end
