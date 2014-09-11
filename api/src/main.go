68eb959d3bde135b55cd4d42f6fc587e604c422f
AuthorOfTheSurf <zanekansil@gmail.com>
2014-09-11 01:28:25 -0700

    Add api struct

--- api/src/main.go
+++ api/src/main.go
@@ -4,6 +4,7 @@ import (
     "github.com/ant0ine/go-json-rest/rest"
     "log"
     "net/http"
+    "gopkg.in/mgo.v2"
 )
 
 func main() {
@@ -11,16 +12,28 @@ func main() {
     handler := rest.ResourceHandler{
         EnableRelaxedContentType: true,
     }
-    err := handler.SetRoutes(
+
+    session, err := mgo.Dial("mongodb://localhost/cher-ami")
+    if err != nil {
+        log.Fatal(err)
+    }
+    api := Api{session}
+
+    err = handler.SetRoutes(
         //&rest.Route{"GET", "/posts", GetAllPosts},
-        //&rest.Route{"POST", "/posts", CreatePost},
-        &rest.Route{"GET", "/posts/:id", GetPost},
+        &rest.Route{"POST", "/posts", api.CreatePost},
+        &rest.Route{"GET", "/posts/:id", api.GetPost},
         //&rest.Route{"DELETE", "/posts/:id", DeletePost},
     )
     if err != nil {
         log.Fatal(err)
     }
+
     log.Fatal(http.ListenAndServe(":8228", &handler))
+}
+
+type Api struct {
+    session *mgo.Session
 }
 
 type Post struct {
@@ -28,7 +41,11 @@ type Post struct {
     Content string
 }
 
-func GetPost(w rest.ResponseWriter, r *rest.Request) {
+func (a Api) CreatePost(w rest.ResponseWriter, r *rest.Request) {
+    // session.DB()
+}
+
+func (a Api) GetPost(w rest.ResponseWriter, r *rest.Request) {
     id := r.PathParam("id")
     post := Post{"Zane", "This is just a sample post with id " + id}
     w.WriteJson(post)
