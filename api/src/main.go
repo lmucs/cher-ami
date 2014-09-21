9c87e89b4632948cacf1df898fdaecc4fbdc61c6
authorofthesurf <zanekansil@gmail.com>
2014-09-20 17:56:18 -0700

    Convert 'GetAllUsers' functionality

--- api/src/main.go
+++ api/src/main.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-    "github.com/ant0ine/go-json-rest/rest"
-    "github.com/jmcvetta/neoism"
-    //"github.com/gorilla/schema"
-    "log"
-    "net/http"
-    "fmt"
-    "time"
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/jmcvetta/neoism"
+	//"github.com/gorilla/schema"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func panicErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func httpError(w rest.ResponseWriter, err error) {
-    if err != nil {
-        rest.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-    port    := "8228"
-    handler := rest.ResourceHandler{
-        EnableRelaxedContentType: true,
-    }
+	port := "8228"
+	handler := rest.ResourceHandler{
+		EnableRelaxedContentType: true,
+	}
 
-    db, err := neoism.Connect("http://localhost:7474/db/data")
-    if  err != nil {
-        log.Fatal(err)
-    }
+	db, err := neoism.Connect("http://localhost:7474/db/data")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    api      := Api{db}
+	api := Api{db}
 
-    err = handler.SetRoutes(
-        &rest.Route{"POST",   "/signup", api.Signup},
-        // &rest.Route{"POST",   "/login", api.Login},
-        &rest.Route{"GET",    "/users", api.GetUser},
-        // &rest.Route{"DELETE", "/users/:id", api.DeleteUser},
-        // &rest.Route{"GET",  "/message", GetAllMessages},
-        // &rest.Route{"POST",   "/messages", api.CreateMessage},
-        // &rest.Route{"GET",    "/messages/:id", api.GetMessage},
-        // &rest.Route{"DELETE", "/messages/:id", api.DeleteMessage},
-    )
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = handler.SetRoutes(
+		&rest.Route{"POST", "/signup", api.Signup},
+		// &rest.Route{"POST",   "/login", api.Login},
+		&rest.Route{"GET", "/users", api.GetUser},
+		// &rest.Route{"DELETE", "/users/:id", api.DeleteUser},
+		// &rest.Route{"GET",  "/message", GetAllMessages},
+		// &rest.Route{"POST",   "/messages", api.CreateMessage},
+		// &rest.Route{"GET",    "/messages/:id", api.GetMessage},
+		// &rest.Route{"DELETE", "/messages/:id", api.DeleteMessage},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Printf("Listening on port %s\n", port)
-    log.Fatal(http.ListenAndServe(":"+port, &handler))
+	fmt.Printf("Listening on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, &handler))
 }
 
 //
@@ -59,7 +59,7 @@ func main() {
 //
 
 type Api struct {
-    db   *neoism.Database
+	db *neoism.Database
 }
 
 //
@@ -85,20 +85,20 @@ type Circle struct {
 }*/
 
 type UserProposal struct {
-    Handle          string
-    Email           string
-    Password        string
-    ConfirmPassword string
+	Handle          string
+	Email           string
+	Password        string
+	ConfirmPassword string
 }
 
 type UserView struct {
-    Handle       string
-    Joined       time.Time
+	Handle string
+	Joined time.Time
 }
 
 type UserSignIn struct {
-    Handle     string
-    Password   string
+	Handle   string
+	Password string
 }
 
 //
@@ -109,62 +109,60 @@ type UserSignIn struct {
  * Expects a json POST with "Username", "Password", "ConfirmPassword"
  */
 func (a Api) Signup(w rest.ResponseWriter, r *rest.Request) {
-    proposal := UserProposal{}
-    err      := r.DecodeJsonPayload(&proposal)
-    if err != nil {
-        rest.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	proposal := UserProposal{}
+	err := r.DecodeJsonPayload(&proposal)
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    stmt := `CREATE (user:User { handle:{handle}, password:{password}, joined: {joined} })
+	stmt := `CREATE (user:User { handle:{handle}, password:{password}, joined: {joined} })
              RETURN user`
 
-    res := []struct {
-        User   neoism.Node
-    }{}
+	res := []struct {
+		User neoism.Node
+	}{}
 
-    params := neoism.Props{
-        "handle": proposal.Handle,
-        "password": proposal.Password,
-        "joined": time.Now().Local(),
-    }
+	params := neoism.Props{
+		"handle":   proposal.Handle,
+		"password": proposal.Password,
+		"joined":   time.Now().Local(),
+	}
 
-    cq := neoism.CypherQuery {
-        Statement: stmt,
-        Parameters: params,
-        Result: &res,
-    }
+	cq := neoism.CypherQuery{
+		Statement:  stmt,
+		Parameters: params,
+		Result:     &res,
+	}
 
-    err = a.db.Cypher(&cq)
-    panicErr(err)
+	err = a.db.Cypher(&cq)
+	panicErr(err)
 
-    // check results
-    if len(res) != 1 {
-        panic(fmt.Sprintf("Incorrect results len in query1()\n\tgot %d, expected 1\n", len(res)))
-    }
+	// check resultsGOPATH
+	if len(res) != 1 {
+		panic(fmt.Sprintf("Incorrect results len in query1()\n\tgot %d, expected 1\n", len(res)))
+	}
 
-    n := res[0].User // Only one row of data returned
-    fmt.Println("createNode()", n.Data)
+	n := res[0].User // Only one row of data returned
+	fmt.Println("createNode()", n.Data)
 
-    // ensure unique handle
-    // count, err := a.db.C("users").Find(bson.M{ "handle": proposal.Handle }).Count()
-    // if count > 0 {
-    //     rest.Error(w, proposal.Handle+" is already taken", 400)
-    //     return
-    // }
-    // if err != nil {
-    //     rest.Error(w, err.Error(), http.StatusInternalServerError)
-    //     return
-    // }
+	// ensure unique handle
+	// count, err := a.db.C("users").Find(bson.M{ "handle": proposal.Handle }).Count()
+	// if count > 0 {
+	//     rest.Error(w, proposal.Handle+" is already taken", 400)
+	//     return
+	// }
+	// if err != nil {
+	//     rest.Error(w, err.Error(), http.StatusInternalServerError)
+	//     return
+	// }
 
-    // password checks
-    // if proposal.Password != proposal.ConfirmPassword {
-    //     rest.Error(w, "Passwords do not match", 400)
-    //     return
-    // }
+	// password checks
+	// if proposal.Password != proposal.ConfirmPassword {
+	//     rest.Error(w, "Passwords do not match", 400)
+	//     return
+	// }
 }
-
-/* Transition to PostgreSQL in progress */
 
 // func (a Api) Login(w rest.ResponseWriter, r *rest.Request) {
 //     credentials := UserSignIn{}
@@ -186,59 +184,58 @@ func (a Api) Signup(w rest.ResponseWriter, r *rest.Request) {
 // }
 
 func (a Api) GetUser(w rest.ResponseWriter, r *rest.Request) {
-    querymap   := r.URL.Query()
+	querymap := r.URL.Query()
 
-    // Get by handle
-    if handle, ok := querymap["handle"]; ok {
-
-        stmt := `MATCH (user:User)
+	// Get by handle
+	if handle, ok := querymap["handle"]; ok {
+		stmt := `MATCH (user:User)
                  WHERE user.handle = {handle}
                  RETURN user`
-        params := neoism.Props{
-            "handle": handle[0],
-        }
-        res := []struct{
-            User   neoism.Node
-        }{}
+		params := neoism.Props{
+			"handle": handle[0],
+		}
+		res := []struct {
+			User neoism.Node
+		}{}
 
-        cq := neoism.CypherQuery{
-            Statement:   stmt,
-            Parameters:  params,
-            Result:      &res,
-        }
+		err := a.db.Cypher(&neoism.CypherQuery{
+			Statement:  stmt,
+			Parameters: params,
+			Result:     &res,
+		})
+		panicErr(err)
 
-        err := a.db.Cypher(&cq)
-        panicErr(err)
+		u := res[0].User.Data
 
-        u := res[0].User.Data
+		w.WriteJson(u)
+		return
+	}
 
-        w.WriteJson(u)
-        return
-    }
+	// All users
+	stmt := `MATCH (user:User)
+             RETURN user.handle, user.joined
+             ORDER BY user.handle`
+	res := []struct {
+		Handle string    `json:"user.handle"`
+		Joined time.Time `json:"user.joined"`
+	}{}
 
-    // // Get by id
-    // if id, ok  := querymap["id"]; ok {
-    //     found  := User{}
-    //     err    := a.db.C("users").
-    //                 Find(bson.M{ "id": bson.ObjectIdHex(id[0]) }).
-    //                 One(&found)
-    //     if err != nil {
-    //         rest.Error(w, err.Error(), http.StatusInternalServerError)
-    //         return
-    //     }
-    //     w.WriteJson(found)
-    //     return
-    // }
+	err := a.db.Cypher(&neoism.CypherQuery{
+		Statement:  stmt,
+		Parameters: neoism.Props{},
+		Result:     &res,
+	})
+	panicErr(err)
 
-
-    // // All users
-    // var users []interface{}
-
-    // a.db.C("users").
-    //     Find(bson.M{}).
-    //     Select(bson.M{ "handle":1 }).
-    //     All(&users)
-
+	if len(res) > 0 {
+		fmt.Println("All users:")
+		for _, user := range res {
+			t := user.Joined.Format(time.RFC1123)
+			fmt.Printf("  %-16s  %-16s\n", user.Handle, t)
+		}
+	} else {
+		fmt.Println("No results found")
+	}
 }
 
 // func (a Api) DeleteUser(w rest.ResponseWriter, r *rest.Request) {
