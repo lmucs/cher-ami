82a3e4cce6dea1d1a43eabb39db67cc49c9c45b7
AbdulZaid <aalzaid1@lion.lmu.edu>
2014-10-05 16:31:22 -0700

    Added a passwordCheck,

--- web/src/static/js/app/views/signup-view.js
+++ web/src/static/js/app/views/signup-view.js
@@ -21,6 +21,7 @@ define(function(require, exports, module) {
 
         events: {
             'click #remember-me': 'onRememberConfirm',
+            'keyup #pass2': 'passwordMatch',
             'click #signup': 'onFormConfirm'
         },
 
@@ -42,7 +43,55 @@ define(function(require, exports, module) {
             });
             console.log(req)
             req.save();
+        },
+
+        passwordMatch: function(event) {
+            //Store the password field objects into variables ...
+            var pass1 = $('#pass1');
+            var pass2 = $('#pass2');
+            //Store the Confimation Message Object ...
+            var message = $('#confirmMessage');
+            //Set the colors we will be using ...
+            var goodColor = "#66cc66";
+            var badColor = "#ff6666";
+            //measure password strength
+            var getStrength = function(password) {
+               var strength = 0;
+               if (pass1.length > 7) {strength += 1};
+               if (pass1.val().match(/([a-zA-Z])/) && pass1.val().match(/([0-9])/))  {strength += 1};
+               if (pass1.val().match(/([!,%,&,@,#,$,^,*,?,_,~])/))  {strength += 1};
+               if (pass1.val().match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) {strength += 1};
+               if(strength < 2) {
+                return 'Weak';
+               } else if(strength == 2) {
+                return 'Strong';
+               } else {
+                return 'Very Strong';
+               }
+            }
+
+            if(pass1.val() == pass2.val() && pass1.val().length >= 8) {
+               pass1.css( "background-color", goodColor);
+               pass2.css( "background-color", goodColor);
+               message.css("color", goodColor);
+               message.text("Passwords Match!" + " " + getStrength(pass1));
+               return true;
+            } else if(pass1.val().length < 8) {
+               pass1.css( "background-color", badColor);
+               pass2.css( "background-color", badColor);
+               message.css("color", badColor);
+               message.text("Password has to be more than 8 charecters");
+            } else {
+               //The passwords do not match.
+               //Set the color to the bad color and
+               //notify the user.
+               pass1.css( "background-color", badColor);
+               pass2.css( "background-color", badColor);
+               message.text("Passwords Do Not Match!");
+               message.innerHTML = "Passwords Do Not Match!"
+            }
         }
+
     });
 
     exports.SignupView = SignupView;
