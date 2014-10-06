78970ccb8cdb71b5ad51320d5d3eff70a7e8cccc
waverill7 <waverill@lion.lmu.edu>
2014-10-06 06:47:48 -0700

    API Testing - Signup and Login

---
+++ go/src/test/api_test.go
@@ -0,0 +1,247 @@
+package api_test
+
+import (
+    api "../api"
+    "fmt"
+    "github.com/jmcvetta/neoism"
+    "io"
+    "log"
+    "net/http"
+    "net/http/httptest"
+    routes "../routes"
+    "strings"
+    "testing"
+)
+
+var (
+    server *httptest.Server
+
+    signupURL string
+    loginURL string
+    logoutURL string
+    usersURL string
+    messagesURL string
+    publishURL string
+    followURL string
+    blockURL string
+    circlesURL string
+
+    reader io.Reader
+)
+
+func setup () {
+	neo4jdb, err := neoism.Connect("http://107.170.229.205:7474/db/data")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a := &api.Api{neo4jdb}
+	handler, err := routes.MakeHandler(*a)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server = httptest.NewServer(&handler)
+
+    signupURL = fmt.Sprintf("%s/signup", server.URL)
+    loginURL = fmt.Sprintf("%s/login", server.URL)
+    logoutURL = fmt.Sprintf("%s/logout", server.URL)
+    usersURL = fmt.Sprintf("%s/users", server.URL)
+    messagesURL = fmt.Sprintf("%s/messages", server.URL)
+    publishURL = fmt.Sprintf("%s/publish", server.URL)
+    followURL = fmt.Sprintf("%s/follow", server.URL)
+    blockURL = fmt.Sprintf("%s/block", server.URL)
+    circlesURL = fmt.Sprintf("%s/circles", server.URL)
+}
+
+func teardown () {
+	server.Close()
+}
+
+func postSignup (handle string, email string, password string, confirmPassword string) (*http.Response, error) {
+	proposal := "{\"Handle\": \"" + handle + "\", \"Email\": \"" + email + "\", \"Password\": \"" + password + "\", \"ConfirmPassword\": \"" + confirmPassword + "\"}"
+	
+	reader = strings.NewReader(proposal)
+	
+	request, err := http.NewRequest("POST", signupURL, reader)
+	
+	response, err := http.DefaultClient.Do(request)
+	
+	return response, err
+}
+
+func postLogin (handle string, password string) (*http.Response, error) {
+	credentials := "{\"Handle\": \"" + handle + "\", \"Password\": \"" + password + "\"}"
+
+	reader = strings.NewReader(credentials)
+
+	request, err := http.NewRequest("POST", loginURL, reader)
+
+	response, err := http.DefaultClient.Do(request)
+
+	return response, err 
+}
+
+func TestSignupEmptyHandle (t *testing.T) {
+	setup()
+	defer teardown()
+
+	response, err := postSignup("", "testing123", "testing123", "testing123")
+	
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.StatusCode != 400 {
+		t.Errorf("HTTP Status Code: %d", response.StatusCode)
+	}
+}
+
+func TestSignupEmptyEmail (t *testing.T) {
+	setup()
+	defer teardown()
+
+	response, err := postSignup("testing123", "", "testing123", "testing123")
+	
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.StatusCode != 400 {
+		t.Errorf("HTTP Status Code: %d", response.StatusCode)
+	}
+}
+
+func TestSignupPasswordMismatch (t *testing.T) {
+	setup()
+	defer teardown()
+
+	response, err := postSignup("testing123", "testing123", "testing123", "testing321")
+	
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.StatusCode != 400 {
+		t.Errorf("HTTP Status Code: %d", response.StatusCode)
+	}
+}
+
+func TestSignupPasswordTooShort (t *testing.T) {
+	setup()
+	defer teardown()
+
+	entry := "testing"
+
+	for i := len(entry); i >= 0; i-- {
+
+	    response, err := postSignup("testing123", "testing123", entry[:len(entry)-i], entry[:len(entry)-i])
+	
+	    if err != nil {
+		    t.Error(err)
+	    }
+
+	    if response.StatusCode != 400 {
+		    t.Errorf("HTTP Status Code: %d, Password Length: %d", response.StatusCode, len(entry)-i)
+	    }
+	}
+}
+
+func TestSignupHandleTaken (t *testing.T) {
+	setup() 
+	defer teardown()
+
+    postSignup("testing123", "testing123", "testing123", "testing123")
+    response, err := postSignup("testing123", "testing321", "testing123", "testing123")
+	
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.StatusCode != 400 {
+		t.Errorf("HTTP Status Code: %d", response.StatusCode)
+	}
+}
+
+func TestSignupEmailTaken (t *testing.T) {
+	setup()
+	defer teardown()
+
+	postSignup("testing123", "testing123", "testing123", "testing123")
+	response, err := postSignup("testing321", "testing123", "testing123", "testing123")
+	
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.StatusCode != 400 {
+		t.Errorf("HTTP Status Code: %d", response.StatusCode)
+	}
+}
+
+func TestSignupCreated (t *testing.T) {
+	setup()
+	defer teardown()
+
+	postSignup("testing123", "testing123", "testing123", "testing123")
+	response, err := postSignup("testing321", "testing321", "testing123", "testing123")
+	
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.StatusCode != 201 {
+		t.Errorf("HTTP Status Code: %d", response.StatusCode)
+	}
+}
+
+func TestLoginInvalidUsername (t *testing.T) {
+    setup()
+    defer teardown()
+
+    postSignup("testing123", "testing123", "testing123", "testing123")
+
+    response, err := postLogin("testing321", "testing123")
+
+    if err != nil {
+    	t.Error(err)
+    }
+
+    if response.StatusCode != 400 {
+    	t.Errorf("HTTP Status Code: %d", response.StatusCode)
+    }
+}
+
+func TestLoginInvalidPassword (t *testing.T) {
+    setup()
+    defer teardown()
+
+    postSignup("testing123", "testing123", "testing123", "testing123")
+
+    response, err := postLogin("testing123", "testing321")
+
+    if err != nil {
+    	t.Error(err)
+    }
+
+    if response.StatusCode != 400 {
+    	t.Errorf("HTTP Status Code: %d", response.StatusCode)
+    }
+}
+
+func TestLoginOK (t *testing.T) {
+	setup()
+	defer teardown()
+
+	postSignup("testing123", "testing123", "testing123", "testing123")
+
+	response, err := postLogin("testing123", "testing123")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.StatusCode != 200 {
+		t.Errorf("HTTP Status Code: %d", response.StatusCode)
+	}
+}
