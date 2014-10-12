28b2d370e6a25f4dc778ace5957ed3917e2056db
waverill7 <waverill@lion.lmu.edu>
2014-10-12 05:36:46 -0700

    Modified JoinDefault to check if target exists.

--- go/src/api/api.go
+++ go/src/api/api.go
@@ -881,6 +881,14 @@ func (a Api) JoinDefault(w rest.ResponseWriter, r *rest.Request) {
 		return
 	}
 
+	if !a.userExists(payload.Target) {
+		w.WriteHeader(400)
+		w.WriteJson(map[string]string{
+			"Response": "Bad request, user " + payload.Target + " wasn't found",
+		})
+		return
+	}
+
 	if a.isBlocked(payload.Handle, payload.Target) {
 		w.WriteHeader(403)
 		w.WriteJson(map[string]string{
