ff205f766a6ac79efd9e31af9bb2c66019da7dfe
AbdulZaid <aalzaid1@lion.lmu.edu>
2014-10-15 04:01:54 -0700

    Added a handle validator

--- web/src/static/js/app/utils/input-validator.js
+++ web/src/static/js/app/utils/input-validator.js
@@ -9,6 +9,28 @@ define(function(require, exports, module) {
 	            validating: 'glyphicon glyphicon-refresh'
 	        },
 	        fields: {
+	        	handle: {
+	        		message: 'The username is not valid',
+	        		validators: {
+	        			notEmpty: {
+                            message: 'The username is required and cannot be empty'
+	                    },
+	                    stringLength: {
+	                        min: 6,
+	                        max: 30,
+	                        message: 'The username must be more than 6 and less than 30 characters long'
+	                    },
+	                    regexp: {
+	                        regexp: /^[a-zA-Z0-9]+$/,
+	                        message: 'The username can only consist of alphabetical and number'
+	                    },
+	                    different: {
+	                        field: 'password',
+	                        message: 'The username and password cannot be the same as each other'
+	                    }
+	        		}
+	        	},
+
 	            email: {
 	                validators: {
 	                    notEmpty: {
@@ -18,7 +40,8 @@ define(function(require, exports, module) {
 	                        message: 'The input is not a valid email address'
 	                    }
 	                }
-	            }
+	            },
+
 	        }
         });
 	}
