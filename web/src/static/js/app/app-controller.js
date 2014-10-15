56b34e5f142bc37f5373116db5733cd72a4a3257
AbdulZaid <aalzaid1@lion.lmu.edu>
2014-10-14 19:52:00 -0700

    Added age to profile page

--- web/src/static/js/app/app-controller.js
+++ web/src/static/js/app/app-controller.js
@@ -29,7 +29,7 @@ define(function(require, exports, module) {
             // this.app.mainRegion.show(new MessagesView({
             //     collection: test
             // }));
-            this.app.mainRegion.show(new CommentsView());
+            this.app.mainRegion.show(new ProfileView());
             // this.app.footerRegion.show(new FooterView());
         },
 
