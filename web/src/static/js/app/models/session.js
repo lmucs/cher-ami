6a9b80d5c70aa5c3727d3479dc9ebebf84584c5a
Edward Bramanti <edward@bramanti.org>
2014-10-30 11:22:07 -0700

    Rewrote logic for sessions

--- web/src/static/js/app/models/session.js
+++ web/src/static/js/app/models/session.js
@@ -1,55 +1,21 @@
 define(function(require, exports, module) {
 
     var backbone = require('backbone');
+    var BackboneSession = require('backbone/sessions');
 
-    var Session = Backbone.Model.extend({
-        url: 'api/sessions',
+    var Session = BackboneSession.extend({
         defaults: {
             handle: null,
             sessionid: null
         },
-        initialize: function() {
-            //this.load();
-            $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-                options.xhrFields = {
-                    withCredentials: true
-                };
-            });
-        },
 
-        /*load: function() {
-            this.model.set({
-                user_id: $.cookie('handle'),
-                sessionid: $.cookie('sessionid')
-            })
-        },*/
-
-        // Takes in a login model
-        login: function(login_model) {
-            console.log(login_model);
-            var that = this;
-            var credentials = login_model.toJSON();
-            this.save(credentials, {
-                success: function(model, response) {
-                    that.unset('password');
-                    that.set(response);
-                    // Removes response property from response object
-                    that.unset('response');
-                }
-            })
-        },
-
-        logout: function() {
-            this.destroy({
-                success: function(model, resp) {
-                    model.clear({silent: true})
-                }
-            })
+        options: {
+            local: true,
+            remote: false,
+            persist: false
         },
 
         authenticated: function() {
-            // Add logic here which fetches auth.
-            return false;
         }
     });
 
