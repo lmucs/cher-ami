9a8e61e0ee2882cb5fd6daffa2d7df19ca81d994
Steve <steve@tgiltd.com>
2014-11-04 10:29:08 -0800

    Now using loopj to perform http requests in java. MUCH BETTER

--- android/CherAmi/app/src/main/java/com/cherami/cherami/SignUpActivity.java
+++ android/CherAmi/app/src/main/java/com/cherami/cherami/SignUpActivity.java
@@ -18,6 +18,10 @@ import android.widget.Toast;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.http.Header;
+import org.apache.http.entity.StringEntity;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -30,12 +34,15 @@ import org.springframework.web.client.RestTemplate;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+
+import com.loopj.android.http.*;
 
 
 public class SignUpActivity extends Activity {
@@ -66,133 +73,190 @@ public class SignUpActivity extends Activity {
     }
 
     public void attemptCreateAccount(View view) {
-        View focusView = null;
-        Boolean cancel = false;
+        AsyncHttpClient client = new AsyncHttpClient();
 
-        String username = mUsername.getText().toString();
-        String email = mEmail.getText().toString();
-        String password = mPassword.getText().toString();
-        String confrimPassword = mConfirmPassword.getText().toString();
-        /* First, data sanitization: No fields should be left blank, email should have @ symbol,
-        password and Confirm password should be the same (this is done in back end)
-        Also, handle/username must be unique (also back end?)
-        Then,
-        POST to db with Handle, Email, Password, and Confirm
-        */
-
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmail.setError(getString(R.string.error_field_required));
-            focusView = mEmail;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmail.setError(getString(R.string.error_invalid_email));
-            focusView = mEmail;
-            cancel = true;
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("handle", mUsername.getText().toString());
+            jsonParams.put("email", mEmail.getText().toString());
+            jsonParams.put("password", mPassword.getText().toString());
+            jsonParams.put("confirmpassword", mConfirmPassword.getText().toString());
+        } catch (JSONException j) {
+            System.out.println("DONT LIKE JSON!");
+        }
+        StringEntity entity = null;
+        try {
+            entity = new StringEntity(jsonParams.toString());
+        } catch (UnsupportedEncodingException i) {
+            System.out.println("DONT LIKE TO STRING!");
         }
 
-        if (TextUtils.isEmpty(password)) {
-            mPassword.setError(getString(R.string.error_field_required));
-            focusView = mPassword;
-            cancel = true;
-        } else if (!isPasswordValid(password)) {
-            mPassword.setError(getString(R.string.error_invalid_password));
-            focusView = mPassword;
-            cancel = true;
-        }
+        client.post(this.getApplicationContext(), "http://10.0.1.29:8228/api/signup", entity, "application/json", new AsyncHttpResponseHandler() {
 
-        if (!confrimPassword.equals(password)) {
-            mConfirmPassword.setError(getString(R.string.error_invalid_confirm));
-            focusView = mConfirmPassword;
-            cancel = true;
-        }
+            @Override
+            public void onStart() {
+                // called before request is started
+                System.out.println("STARTING POST REQUEST");
 
-        if (TextUtils.isEmpty(username)) {
-            mUsername.setError(getString(R.string.error_field_required));
-            focusView = mUsername;
-            cancel = true;
-        }
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                String s = new String(response);
+
+                // called when response HTTP status is "200 OK"
+                System.out.println("SUCCESS IN POSTING THAT USER!");
+                System.out.println("status code: " + statusCode);
+                System.out.println("response: " + s);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                String s = new String(errorResponse);
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                System.out.println("AWE FUCK");
+                System.out.println("status code: " + statusCode);
+                System.out.println("response: " + s);
+                e.printStackTrace();
+
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+                System.out.println("RETRYING?!?!");
+            }
+        });
 
 
-        if (cancel) {
-            //Something is wrong; don't sign up
-            focusView.requestFocus();
-        } else {
-            // Sign them up
-            new HttpRequestTask().execute(MediaType.APPLICATION_JSON);
 
-        }
+//        View focusView = null;
+//        Boolean cancel = false;
+//
+//        String username = mUsername.getText().toString();
+//        String email = mEmail.getText().toString();
+//        String password = mPassword.getText().toString();
+//        String confrimPassword = mConfirmPassword.getText().toString();
+//        /* First, data sanitization: No fields should be left blank, email should have @ symbol,
+//        password and Confirm password should be the same (this is done in back end)
+//        Also, handle/username must be unique (also back end?)
+//        Then,
+//        POST to db with Handle, Email, Password, and Confirm
+//        */
+//
+//        // Check for a valid email address.
+//        if (TextUtils.isEmpty(email)) {
+//            mEmail.setError(getString(R.string.error_field_required));
+//            focusView = mEmail;
+//            cancel = true;
+//        } else if (!isEmailValid(email)) {
+//            mEmail.setError(getString(R.string.error_invalid_email));
+//            focusView = mEmail;
+//            cancel = true;
+//        }
+//
+//        if (TextUtils.isEmpty(password)) {
+//            mPassword.setError(getString(R.string.error_field_required));
+//            focusView = mPassword;
+//            cancel = true;
+//        } else if (!isPasswordValid(password)) {
+//            mPassword.setError(getString(R.string.error_invalid_password));
+//            focusView = mPassword;
+//            cancel = true;
+//        }
+//
+//        if (!confrimPassword.equals(password)) {
+//            mConfirmPassword.setError(getString(R.string.error_invalid_confirm));
+//            focusView = mConfirmPassword;
+//            cancel = true;
+//        }
+//
+//        if (TextUtils.isEmpty(username)) {
+//            mUsername.setError(getString(R.string.error_field_required));
+//            focusView = mUsername;
+//            cancel = true;
+//        }
+//
+//
+//        if (cancel) {
+//            //Something is wrong; don't sign up
+//            focusView.requestFocus();
+//        } else {
+//            // Sign them up
+//            new HttpRequestTask().execute(MediaType.APPLICATION_JSON);
+//
+//        }
 
     }
 
 
-    private class HttpRequestTask extends AsyncTask<MediaType, Void, String> {
-        private NewUser u;
-        private String host;
-
-        @Override
-        protected void onPreExecute() {
-            u = new NewUser(mUsername.getText().toString(), mEmail.getText().toString(), mPassword.getText().toString(), mConfirmPassword.getText().toString());
-
-            AssetManager assetManager = getResources().getAssets();
-            InputStream inputStream = null;
-            try {
-                inputStream = assetManager.open("config.properties");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Properties properties = new Properties();
-            try {
-                properties.load(inputStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            host = properties.getProperty("myUrl");
-        }
-
-
-        @Override
-        protected String doInBackground(MediaType... params) {
-            try {
-                final String url = host + "/api/signup";
-
-
-                // Set the Content-Type header
-                HttpHeaders requestHeaders = new HttpHeaders();
-                requestHeaders.setContentType(MediaType.APPLICATION_JSON);
-                HttpEntity<NewUser> requestEntity = new HttpEntity<NewUser>(u, requestHeaders);
-
-// Create a new RestTemplate instance
-                RestTemplate restTemplate = new RestTemplate();
-
-// Add the Jackson and String message converters
-                restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-
-
-// Make the HTTP POST request, marshaling the request to JSON, and the response to a String
-                ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
-                return response.getBody();
-
-            } catch (Exception e) {
-                Log.e("MainActivity", e.getMessage(), e);
-//                if (e instanceof MyCustomException) {
-//                    MyCustomException exception = (MyCustomException) e;
-//                    Log.d("Error: ", "An error occurred while calling api/user/account API endpoint: " + e.getMessage());
-//                } else {
-//                    Log.d("Error: ", "An error occurred while trying to parse Login Response JSON object");
-//                }
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Toast toast = Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG);
-            toast.show();
-        }
-    }
+//    private class HttpRequestTask extends AsyncTask<MediaType, Void, String> {
+//        private NewUser u;
+//        private String host;
+//
+//        @Override
+//        protected void onPreExecute() {
+//            u = new NewUser(mUsername.getText().toString(), mEmail.getText().toString(), mPassword.getText().toString(), mConfirmPassword.getText().toString());
+//
+//            AssetManager assetManager = getResources().getAssets();
+//            InputStream inputStream = null;
+//            try {
+//                inputStream = assetManager.open("config.properties");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            Properties properties = new Properties();
+//            try {
+//                properties.load(inputStream);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            host = properties.getProperty("myUrl");
+//        }
+//
+//
+//        @Override
+//        protected String doInBackground(MediaType... params) {
+//            try {
+//                final String url = host + "/api/signup";
+//
+//
+//                // Set the Content-Type header
+//                HttpHeaders requestHeaders = new HttpHeaders();
+//                requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+//                HttpEntity<NewUser> requestEntity = new HttpEntity<NewUser>(u, requestHeaders);
+//
+//                // Create a new RestTemplate instance
+//                RestTemplate restTemplate = new RestTemplate();
+//
+//                // Add the Jackson and String message converters
+//                restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+//                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+//
+//
+//                // Make the HTTP POST request, marshaling the request to JSON, and the response to a String
+//                ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+//                return response.getBody();
+//
+//            } catch (Exception e) {
+//                Log.e("MainActivity", e.getMessage(), e);
+////                if (e instanceof MyCustomException) {
+////                    MyCustomException exception = (MyCustomException) e;
+////                    Log.d("Error: ", "An error occurred while calling api/user/account API endpoint: " + e.getMessage());
+////                } else {
+////                    Log.d("Error: ", "An error occurred while trying to parse Login Response JSON object");
+////                }
+//            }
+//
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//            Toast toast = Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG);
+//            toast.show();
+//        }
+//    }
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
