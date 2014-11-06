1c8d97e4d61274a8d1bbbcdc8b7a53f30db2edb7
AbdulZaid <aalzaid1@lion.lmu.edu>
2014-11-05 16:03:38 -0800

    fixed the template content to be used in the layout.js

--- web/src/static/js/app/layouts/landing-layout.js
+++ web/src/static/js/app/layouts/landing-layout.js
@@ -2,13 +2,15 @@ define(function(require, exports, module) {
 
     var marionette = require('marionette');
     var template = require('hbs!../templates/layouts/landing-layout')
-
+    var SignupView = require('app/views/signup-view').SignupView;
+    var LoginView = require('app/views/login-view').LoginView;
+    
     var LandingLayout = marionette.LayoutView.extend({
         template: template,
 
         regions: {
-            signup: '#signUpContainer',
-            login: '#loginContainer'
+            signup: '#signup',
+            login: '#login'
         },
 
         initialize: function(options) {
