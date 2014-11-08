736b2ec57256be566809f36266a4846131eb94dc
AbdulZaid <aalzaid1@lion.lmu.edu>
2014-11-08 15:59:31 -0800

    Still working on the messages and trying to show load the,

--- web/src/static/js/app/layouts/home-layout.js
+++ web/src/static/js/app/layouts/home-layout.js
@@ -4,7 +4,9 @@ define(function(require, exports, module) {
     var template = require('hbs!../templates/layouts/home-layout')
     var SidebarView = require('app/views/sidebar-view').SidebarView; 
     var MessagesView = require('app/views/messages-view').MessagesView; 
-    var MessageView = require('app/views/message-view').MessageView; 
+    var MessageView = require('app/views/message-view').MessageView;
+    var Message = require('app/models/message').Message;
+    var Messages = require('app/collections/messages').Messages;
 
     var HomeLayout = marionette.LayoutView.extend({
         template: template,
@@ -16,6 +18,7 @@ define(function(require, exports, module) {
 
         ui: {
             sidebarContainer: '#sidebar-container',
+            feedContainer: '#feed-container',
             showContent: '#showContent'
         },
 
@@ -25,8 +28,16 @@ define(function(require, exports, module) {
 
         showContent: function(options) { 
             var sidebar = new SidebarView();
+            var messages = new Messages();
+            var newMessages = new MessagesView();
+            var feed = new MessagesView({
+                    collection: messages
+            });
+
+            this.ui.feedContainer.html(feed.el);
             this.ui.sidebarContainer.html(sidebar.el);
-            console.log(sidebar.el);
+            console.log(feed.el);
+            //messages.render();
             sidebar.render();
         },
 
