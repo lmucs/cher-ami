e6ce1459dc489b44f28f2b6089554791a8bbb5d6
AbdulZaid <aalzaid1@lion.lmu.edu>
2014-11-08 15:34:36 -0800

    home-layout showing sidebar Yaay

--- web/src/static/js/app/app-controller.js
+++ web/src/static/js/app/app-controller.js
@@ -26,6 +26,7 @@ define(function(require, exports, module) {
     var CreateCircleView = require('app/views/create-circle-view').CreateCircleView;
 
     var LandingLayout = require('app/layouts/landing-layout').LandingLayout;
+    var HomeLayout = require('app/layouts/home-layout').HomeLayout;
 
     var AppController = marionette.Controller.extend({
 
@@ -57,7 +58,7 @@ define(function(require, exports, module) {
             // Initialization of views will go here.
             this.app.headerRegion.show(new HeaderView());
             //this.app.mainRegion.show(new SidebarView());
-            this.app.mainRegion.show(new LoginView());
+            this.app.mainRegion.show(new HomeLayout());
             //this.app.mainRegion.show(new CircleView());
         },
 
--- web/src/static/js/app/layouts/home-layout.js
+++ web/src/static/js/app/layouts/home-layout.js
@@ -2,12 +2,32 @@ define(function(require, exports, module) {
 
     var marionette = require('marionette');
     var template = require('hbs!../templates/layouts/home-layout')
+    var SidebarView = require('app/views/sidebar-view').SidebarView; 
+    var MessagesView = require('app/views/messages-view').MessagesView; 
+    var MessageView = require('app/views/message-view').MessageView; 
 
     var HomeLayout = marionette.LayoutView.extend({
         template: template,
 
         regions: {
+            sidebarContainer: '#sidebar-container',
+            feedContainer: '#feed-container'
+        },
 
+        ui: {
+            sidebarContainer: '#sidebar-container',
+            showContent: '#showContent'
+        },
+
+        events: {
+            'click #showContent': 'showContent'
+        },
+
+        showContent: function(options) { 
+            var sidebar = new SidebarView();
+            this.ui.sidebarContainer.html(sidebar.el);
+            console.log(sidebar.el);
+            sidebar.render();
         },
 
         initialize: function(options) {
