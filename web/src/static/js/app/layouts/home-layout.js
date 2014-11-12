bb6bb4974de54a68b74d81194c39d79a218ccea0
atsullivan <atsullivan23@gmail.com>
2014-11-11 19:15:00 -0800

    Linked Profile Page to sidebar

--- web/src/static/js/app/layouts/home-layout.js
+++ web/src/static/js/app/layouts/home-layout.js
@@ -7,29 +7,32 @@ define(function(require, exports, module) {
     var Message = require('app/models/message').Message;
     var Messages = require('app/collections/messages').Messages;
     var CreateCircleView = require('app/views/create-circle-view').CreateCircleView;
+    var ProfileView = require('app/views/profile-view').ProfileView;
 
     var HomeLayout = marionette.LayoutView.extend({
         template: template,
 
         regions: {
             feed: '#feed-container',
-            circle: '#create-circle-view'
+            circle: '#create-circle-view',
+            profile: '#profile-container'
         },
 
         ui: {
             feedContainer: '#feed-container',
             showContent: '#showContent',
-            createCircle: '#goToCreateCircle'
+            createCircle: '#goToCreateCircle',
+            displayProfile: '#goToProfile'
         },
 
         events: {
-            'click #goToCreateCircle': 'showCreateCircle'
+            'click #goToCreateCircle': 'showCreateCircle',
+            'click #goToProfile': 'showProfile'
         },
 
         initialize: function(options) {
         },
 
-        
 
         onRender: function() {
             var messages = new Messages();
@@ -38,6 +41,12 @@ define(function(require, exports, module) {
             });
             this.feed.show(feed);
         },
+
+        showProfile: function(options) {
+            var showProfile = new ProfileView();
+            this.profile.show(showProfile);
+        },
+
         showCreateCircle: function(options) {
             var createCircle = new CreateCircleView();
             this.circle.show(createCircle);
