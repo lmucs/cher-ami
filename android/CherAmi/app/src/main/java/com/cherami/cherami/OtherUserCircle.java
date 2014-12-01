5cac5bbfc89539b904b116eb86ddc1b6ed0c7279
ThatHalfKorean <gkim6@lion.lmu.edu>
2014-12-01 11:04:58 -0800

    Implemented search result

---
+++ android/CherAmi/app/src/main/java/com/cherami/cherami/OtherUserCircle.java
@@ -0,0 +1,16 @@
+package com.cherami.cherami;
+
+/**
+ * Created by Geoff on 11/30/2014.
+ */
+public class OtherUserCircle {
+    public String name;
+    public String owner;
+    public String date;
+
+    public OtherUserCircle(String name, String owner, String date) {
+        this.name = name;
+        this.owner = owner;
+        this.date = date;
+    }
+}
