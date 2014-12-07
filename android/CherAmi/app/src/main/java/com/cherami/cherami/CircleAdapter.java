1b42c544ad683a12da1fb0e9d9d4141cd26f0fe2
ThatHalfKorean <gkim6@lion.lmu.edu>
2014-12-07 12:03:26 -0800

    differentiated between public and private circles

--- android/CherAmi/app/src/main/java/com/cherami/cherami/CircleAdapter.java
+++ android/CherAmi/app/src/main/java/com/cherami/cherami/CircleAdapter.java
@@ -2,6 +2,7 @@ package com.cherami.cherami;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,7 +46,19 @@ public class CircleAdapter extends ArrayAdapter<Circle> {
         }
 
         Circle circle = data[position];
+
         try {
+            if(circle.circle.getString("visibility").equals("private")){
+                row.setBackgroundColor(Color.parseColor("#4cc1f0"));
+                holder.txtName.setTextColor(Color.parseColor("#ffffff"));
+                holder.txtOwner.setTextColor(Color.parseColor("#ffffff"));
+                holder.txtDate.setTextColor(Color.parseColor("#ffffff"));
+            } else {
+                row.setBackgroundColor(Color.parseColor("#f0f0f0f0"));
+                holder.txtName.setTextColor(Color.parseColor("#000000"));
+                holder.txtOwner.setTextColor(Color.parseColor("#000000"));
+                holder.txtDate.setTextColor(Color.parseColor("#000000"));
+            }
             holder.txtName.setText(circle.circle.getString("name"));
             holder.txtOwner.setText(circle.circle.getString("owner"));
             holder.txtDate.setText(circle.circle.getString("created"));
